47178aed6cf81533b0d1ae2fde37c122ea4f9120
hykim02 <hykim4435@naver.com>
2024-07-15 00:19:08 +0900

    Success

--- 기초-리스트/basic6096.py
+++ 기초-리스트/basic6096.py
@@ -4,24 +4,19 @@ a = []
 
  # 바둑알이 깔려 있는 상황이 19 * 19 크기의 정수값으로 입력
 for i in range(19):
-    b = input("현재 바둑판 입력:").split()
+    b = input().split()
 
     for j in range(19):
         b[j] = int(b[j])
     
     a.append(b)
 
-for x in range(19):
-    for y in range(19):
-        print(a[x][y], end = ' ')
-    print()
-
 # 십자 뒤집기 횟수(n)가 입력
-num = int(input("뒤집기 횟수:"))
+num = int(input())
 
 # 십자 뒤집기 좌표가 횟수(n) 만큼 입력된다. 단, n은 10이하의 자연수
 for i in range(num):
-    x, y = map(int, input("좌표:").split())
+    x, y = map(int, input().split())
 
     # 가로줄 십자 뒤집기
     for h in range(19):
@@ -38,7 +33,7 @@ for i in range(num):
             a[v][y-1] = 1
 
 # 바둑판 출력
-for x in range(1, 19):
-    for y in range(1, 19):
+for x in range(19):
+    for y in range(19):
         print(a[x][y], end = ' ')
     print()
--- 기초-리스트/basic6097.py
+++ 기초-리스트/basic6097.py
@@ -3,7 +3,7 @@
 # 격자판의 세로(h), 가로(w) 가 공백을 두고 입력
 h, w = map(int, input("세로, 가로:").split())
 
-# 격자판 출력
+# 격자판 생성
 b = []
 
 for i in range(h):
@@ -11,21 +11,40 @@ for i in range(h):
     for j in range(w):
         b[i].append(0)
         
+
+# 격자판 출력
 for i in range(h):
     for j in range(w):
         print(b[i][j], end = " ")
     print()
 
 # 놓을 수 있는 막대의 개수(n)
-n = int(input("막대 개수:"))
+n = input("막대 개수:")
+n = int(n)
 
 # 각 막대의 길이(l), 방향(d: 가로는 0, 세로는 1), 좌표(x, y)가 입력
 for i in range(n):
     l, d, x, y = map(int, input("길이, 방향, 좌표(x, y):").split())
+    b[x-1][y-1] = 1 # 첫좌표 먼저 표시
 
     # 격자판 출력
-    
+    if(d == 0): # 가로인 경우
+        y = y-1
+        print("y:", y)
+        for j in range(l-1): # 길이만큼 반복
+            y += 1
+            print("y:", y)
+            b[x-1][y] = 1
+    else: # 세로인 경우
+        x = x-1
+        print("x:", x)
+        for j in range(l-1): # 길이만큼 반복
+            x += 1
+            print("x:", x)
+            b[x][y-1] = 1
 
-
-
-
+# 격자판 출력
+for i in range(h):
+    for j in range(w):
+        print(b[i][j], end = " ")
+    print()
